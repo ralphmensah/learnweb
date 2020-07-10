4e7ef92e0eb18793face4c81fd1adab9e76e5b30
pkerbynn <john.erbynn@gmail.com>
2020-07-10 14:14:43 +0000

    + object as arrayList

--- src/io/turntabl/Car.java
+++ src/io/turntabl/Car.java
@@ -2,25 +2,34 @@ package io.turntabl;
 
 public class Car {
 //    this
-   private String color;
+   private String type;
    private String model;
    private int speedLevel;
 
    public Car(String color, String model, int speedLevel){
-       this.color = color;
+       this.type = color;
        this.model = model;
        this.speedLevel = speedLevel;
    }
 
-   public String getColor(){
-       return this.color;
-   }
+    public String getType() {
+        return type;
+    }
 
-   public String horn(){
-       System.out.println("peeee...");
-       return "peeep!!";
-   }
-   public void move(){
-       System.out.println("moving...");
-   }
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpeedLevel() {
+        return speedLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + type + '\'' +
+                ", model='" + model + '\'' +
+                ", speedLevel=" + speedLevel +
+                '}';
+    }
 }
--- src/io/turntabl/Main.java
+++ src/io/turntabl/Main.java
@@ -1,12 +1,31 @@
 package io.turntabl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Car benze = new Car("red", "mkd2020", 200);
+        List<Car> carList = new ArrayList<Car>(Arrays.asList(
+                new Car("Benze", "A234", 300),
+                new Car("Picanto", "B234", 400)
+        ));
 
-        System.out.println("benze color: " + benze.getColor());
-        System.out.printf("benze horn: " +  benze.horn());
+        System.out.println("Car list >> " + carList);
+        System.out.println("lenght : " + carList.size());
+
+        Car taxi = new Car("taxi", "E3545", 150);
+        carList.add(taxi);
+
+        System.out.println("Car list 2 : " + carList);
+        System.out.println("length: " + carList.size());
+
+        System.out.println("for each loop..");
+        for (Car car: carList) {
+            System.out.println(car.getType());
+        }
+
     }
 }
